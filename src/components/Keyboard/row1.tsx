9fe6260892eacb020c17ce54880c5529e7cc6245
Your Name <you@example.com>
2024-01-27 17:11:57 +0530

    keys div FC added

--- src/components/Keyboard/row1.tsx
+++ src/components/Keyboard/row1.tsx
@@ -2,16 +2,20 @@
 
 const RowData1 = ["Q", "w" ,"E", "R", "T", "Y", "U", "I", "O" ,"P"];
 
+const Key = ({ele}:{ele:string})=>{
+    return (
+    <div>
+        {ele}
+    </div>
+    ) 
+}
+
 const Row1 = () => {
   return (
     <div>
         {
             RowData1.map( (ele) => {
-                return <div>
-                    {
-                        ele
-                    }
-                </div> 
+                return <Key ele={ele}/>
             })
         }
     </div>
